07cddbcfcd31056d2dc3c3ec39d04ef9ddf421c1
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-17 20:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let innerInput = document.getElementById('text');
+let delayInput = document.getElementById('delay');
+let btn = document.getElementById('btn');
+
+btn.document.addEventListener('click', async () =>{
+	let text = textInput.value;
+	let delay = delayInput.value;
+
+	await new Promise (resolve => setTimeout(resolve, delay));
+
+	outputDiv.textContent = text;
+})
